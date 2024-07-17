f75efef0401eba94e63f75413c645f6c048247fc
Kasuma <113836758+kasumabalidps@users.noreply.github.com>
2024-07-17 16:11:07 +0800

    Update Halamanuser.vue

--- components/Halamanuser.vue
+++ components/Halamanuser.vue
@@ -1,18 +1,40 @@
 <template>
     <div class="flex flex-col w-full p-6">
-      <header class="mb-4">
+      <header class="mb-4 pt-4">
         <div class="flex justify-between items-center mb-2">
-          <h2 class="text-2xl font-bold text-white">HALAMAN USER</h2>
+          <h2 class="text-2xl font-bold text-white">Halaman Users</h2>
           <span class="text-white">{{ currentDate }}</span>
         </div>
         <hr class="border-gray-300">
       </header>
       <main class="flex-grow bg-gray-900 p-6 rounded-lg shadow-lg">
-        <p class="text-white">Konten halaman user akan ditampilkan di sini.</p>
-      </main>
+      <div class="overflow-x-auto relative rounded-md">
+        <table class="w-full text-sm text-left text-gray-400 ">
+          <thead class="text-xs text-white uppercase bg-gray-800">
+            <tr>
+              <th scope="col" class="py-3 px-6">Username</th>
+              <th scope="col" class="py-3 px-6">Point</th>
+              <th scope="col" class="py-3 px-6">Level</th>
+              <th scope="col" class="py-3 px-6">Soal Selesai</th>
+              <th scope="col" class="py-3 px-6">XP</th>
+              <th scope="col" class="py-3 px-6">Tanggal History Terakhir</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="bg-gray-800 border-b border-gray-800">
+              <td class="py-4 px-6">User1</td>
+              <td class="py-4 px-6">1500</td>
+              <td class="py-4 px-6">5</td>
+              <td class="py-4 px-6">50</td>
+              <td class="py-4 px-6">2000</td>
+              <td class="py-4 px-6">2024-07-16</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </main>
     </div>
   </template>
-
 <script>
 export default {
   data() {
