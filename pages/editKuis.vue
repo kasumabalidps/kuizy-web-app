715ab96bd98ad854d9de4fa70c2318e7cdbed470
Kasuma <113836758+kasumabalidps@users.noreply.github.com>
2024-07-18 11:30:30 +0800

    Update editKuis.vue

--- pages/editKuis.vue
+++ pages/editKuis.vue
@@ -36,48 +36,39 @@
                   <ul v-if="dropdownOpen === index" class="absolute bg-gray-800 rounded-lg mt-2 shadow-md py-1 w-32 z-10">
                     <li @click="openModal(quiz.name)" class="text-white px-4 py-2 cursor-pointer hover:bg-gray-700">Tambah Kuis</li>
                     <li class="text-white px-4 py-2 cursor-pointer hover:bg-gray-700">Hapus Kuis</li>
-                </ul>
+                  </ul>
                 </td>
               </tr>
             </tbody>
           </table>
         </div>
-
-        <div v-if="isModalOpen" class="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center z-50">
-          <div class="bg-gray-800 rounded-lg p-6 w-1/2">
-            <div class="flex justify-between items-center mb-4">
-              <h3 class="text-xl font-bold text-white">{{ currentQuiz }}</h3>
-              <button @click="closeModal" class="text-gray-500 hover:text-gray-700 text-3xl">
-                &times;
-              </button>
-            </div>
-            <div class="mb-4">
-              <input type="text" class="w-full p-2 border border-gray-500 rounded mt-2 bg-gray-700 text-white" placeholder="Masukkan Soal">
-            </div>
-            <div class="mb-2">
-              <div class="flex flex-col space-y-2">
-                <label class="flex items-center">
-                  <input type="radio" class="mr-2" name="answer" value="1">
-                  <input type="text" class="w-full p-2 border border-gray-500 rounded bg-gray-700 text-white" placeholder="Opsi 1">
-                </label>
-                <label class="flex items-center">
-                  <input type="radio" class="mr-2" name="answer" value="2">
-                  <input type="text" class="w-full p-2 border border-gray-500 rounded bg-gray-700 text-white" placeholder="Opsi 2">
-                </label>
-                <label class="flex items-center">
-                  <input type="radio" class="mr-2" name="answer" value="3">
-                  <input type="text" class="w-full p-2 border border-gray-500 rounded bg-gray-700 text-white" placeholder="Opsi 3">
-                </label>
-                <label class="flex items-center">
-                  <input type="radio" class="mr-2" name="answer" value="4">
-                  <input type="text" class="w-full p-2 border border-gray-500 rounded bg-gray-700 text-white" placeholder="Opsi 4">
-                </label>
-                <p class="flex mr-2 text-gray-400 text-sm">Note: Check radio button untuk memilih mana jawaban yang benar</p>
+        <div v-if="isModalOpen" class="fixed inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center z-50 overflow-y-auto">
+          <div class="bg-gray-800 rounded-lg p-6 w-[70%] max-h-[75vh] overflow-y-auto relative flex flex-col">
+            <div class="flex-grow">
+              <div class="flex justify-between items-center mb-4">
+                <h3 class="text-xl font-bold text-white">{{ currentQuiz }}</h3>
+                <button @click="closeModal" class="text-gray-500 hover:text-gray-700 text-3xl">
+                  &times;
+                </button>
+              </div>
+              <div v-for="(question, index) in questions" :key="index" class="mb-2">
+                <input v-model="question.questionText" type="text" class="w-full p-2 border border-gray-500 rounded mt-2 bg-gray-700 text-white" placeholder="Masukkan Soal">
+                <div class="flex flex-col space-y-2 mt-2">
+                  <label class="flex items-center" v-for="(option, oIndex) in question.options" :key="oIndex">
+                    <input type="radio" class="mr-2" :name="'answer-' + index" :value="oIndex">
+                    <input v-model="option.text" type="text" class="w-full p-2 border border-gray-500 rounded bg-gray-700 text-white" :placeholder="'Opsi ' + (oIndex + 1)">
+                  </label>
+                </div>
               </div>
             </div>
-            <div class="flex justify-between items-center mt-4">
-              <button class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600">Tambah Soal</button>
-              <button class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">Upload Kuis</button>
+            <div class="p-6 bg-gray-800"> 
+              <div class="flex justify-between items-center">
+                <span class="text-white">Jumlah Soal: {{ questions.length }}</span>
+                <div>
+                  <button @click="addQuestion" class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 mr-3">Tambah Soal</button>
+                  <button class="bg-green-500 text-white px-4 py-2 rounded hover:bg-green-600">Upload Kuis</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
@@ -85,51 +76,57 @@
     </div>
   </div>
 </template>
-
 <script>
-  export default {
-    data() {
-      return {
-        currentDate: '',
-        isModalOpen: false,
-        currentQuiz: '', 
-        dropdownOpen: -1, 
-        quizzes: [
-          { name: 'Matematika', logo: 'matematika.png', numQuizzes: 1, numQuestions: 10 },
-          { name: 'Ilmu Pengetahuan Alam', logo: 'ipa.png', numQuizzes: 1, numQuestions: 10 },
-          { name: 'Ilmu Pengetahuan Sosial', logo: 'ips.png', numQuizzes: 1, numQuestions: 10 },
-          { name: 'Pendidikan Kewarnegaraan', logo: 'pkn.png', numQuizzes: 1, numQuestions: 10 },
-        ],
+export default {
+  data() {
+    return {
+      currentDate: '',
+      isModalOpen: false,
+      currentQuiz: '', 
+      dropdownOpen: -1, 
+      quizzes: [
+        { name: 'Matematika', logo: 'matematika.png', numQuizzes: 1, numQuestions: 10 },
+        { name: 'Ilmu Pengetahuan Alam', logo: 'ipa.png', numQuizzes: 1, numQuestions: 10 },
+        { name: 'Ilmu Pengetahuan Sosial', logo: 'ips.png', numQuizzes: 1, numQuestions: 10 },
+        { name: 'Pendidikan Kewarnegaraan', logo: 'pkn.png', numQuizzes: 1, numQuestions: 10 },
+      ],
+      questions: [],
+    };
+  },
+  methods: {
+    getCurrentDate() {
+      const date = new Date();
+      const options = {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
       };
+      this.currentDate = date.toLocaleDateString('id-ID', options);
     },
-    methods: {
-      getCurrentDate() {
-        const date = new Date();
-        const options = {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric'
-        };
-        this.currentDate = date.toLocaleDateString('id-ID', options);
-      },
-      openModal(quizName) {
-        this.isModalOpen = true;
-        this.currentQuiz = quizName;
-      },
-      closeModal() {
-        this.isModalOpen = false;
-      },
-      toggleDropdown(index) {
-        if (this.dropdownOpen === index) {
-          this.dropdownOpen = -1; 
-        } else {
-          this.dropdownOpen = index; 
-        }
-      },
+    openModal(quizName) {
+      this.isModalOpen = true;
+      this.currentQuiz = quizName;
+      this.questions = []; // Reset questions when opening a new modal
     },
-    mounted() {
-      this.getCurrentDate();
-    }
-  };
+    closeModal() {
+      this.isModalOpen = false;
+    },
+    toggleDropdown(index) {
+      if (this.dropdownOpen === index) {
+        this.dropdownOpen = -1; 
+      } else {
+        this.dropdownOpen = index; 
+      }
+    },
+    addQuestion() {
+      this.questions.push({
+        questionText: "",
+        options: [{text: ""}, {text: ""}, {text: ""}, {text: ""}]
+      });
+    },
+  },
+  mounted() {
+    this.getCurrentDate();
+  }
+};
 </script>
-
